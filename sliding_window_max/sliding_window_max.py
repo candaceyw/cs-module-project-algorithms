13586f17c4794d003d295e49941b055b8c5374c8
candacewilson <candaceyw@gmail.com>
2020-07-16 21:44:16 -0400

    cookie monster complete

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -4,26 +4,50 @@ Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
     # Your code here
-    output = []
-    dq = []
+    output_arr = []
 
-    # length of dq <= k
-    # values store are indexes
-    # max contained dq[0]
-    for i, num in enumerate(nums):
-        if dq and dq[0] < i - k + 1:
-            dq.pop(0)
+    for i in range(len(nums) - k +1):
+        current_window = nums[i:i+k]
+        current_max = max(current_window)
+        output_arr.append(current_max)
+    return output_arr
 
-        # while dq exists
-        while dq and nums[dq[-1]] < num:
-            dq.pop()
 
-        dq.append(i)
+    # output = []
+    # dq = []
+    #
+    # # length of dq <= k
+    # # values store are indexes
+    # # max contained dq[0]
+    # for i, num in enumerate(nums):
+    #     if dq and dq[0] < i - k + 1:
+    #         dq.pop(0)
+    #
+    #     # while dq exists
+    #     while dq and nums[dq[-1]] < num:
+    #         dq.pop()
+    #
+    #     dq.append(i)
+    #
+    #     if i >= k -1:
+    #         output.append(nums[dq[0]])
+    #
+    # return output
 
-        if i >= k -1:
-            output.append(nums[dq[0]])
+def sliding_windo_max_queue(nums, k):
+    # create a queue that stores "useful numbers"
+    # insert the first K elements
+        # for each element we add
+        # all smaller numbers in the queue, remove them
 
-    return output
+        # add the number to the end of the queue
+
+    # Process the remaining elements in nums
+    # from K to n-1
+        # the elements at the front of the queue
+        # is the largest number of the current window
+        # so save that number into our output
+
 
 
 if __name__ == '__main__':
